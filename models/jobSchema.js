df01c6d5bac6ee76203a57cd3a720bf50a65a0d7
Bisma-Aslam <bismamalik2056@gmail.com>
2024-05-14 10:22:40 +0500

    References fixed

--- models/jobSchema.js
+++ models/jobSchema.js
@@ -1,7 +1,29 @@
 const mongoose = require("mongoose");
 
 const EmployerSchema = new mongoose.Schema({
-    
+    jobTitle: {
+        type: String,
+        required: true
+    },
+    companyName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    jobType: {
+        type: String,
+        required: true
+    },
+    jobDescription: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+    },
+    date: {
+        type: Date,
+    }
 });
 
 const EmployerModel = mongoose.model("Employer", EmployerSchema);
